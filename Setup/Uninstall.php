6d917008a7ad384803fb3ff94b84fc53740cceac
Marius Strajeru <marius.strajeru@gmail.com>
2016-06-30 10:36:50 +0300

    add i18n & uninstall files

---
+++ Setup/Uninstall.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Sample_News extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category  Sample
+ * @package   Sample_News
+ * @copyright 2016 Marius Strajeru
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
+ * @author    Marius Strajeru
+ */
+namespace Sample\News\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+use Magento\Config\Model\ResourceModel\Config\Data;
+use Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory;
+
+
+/**
+ * @codeCoverageIgnore
+ */
+class Uninstall implements UninstallInterface
+{
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+    /**
+     * @var Data
+     */
+    protected $configResource;
+
+    /**
+     * @param CollectionFactory $collectionFactory
+     * @param Data $configResource
+     */
+    public function __construct(
+        CollectionFactory $collectionFactory,
+        Data $configResource
+    )
+    {
+        $this->collectionFactory = $collectionFactory;
+        $this->configResource    = $configResource;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @SuppressWarnings(PHPMD.Generic.CodeAnalysis.UnusedFunctionParameter)
+     */
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        //remove tables
+        if ($setup->tableExists('sample_news_author_store')) {
+            $setup->getConnection()->dropTable('sample_news_author_store');
+        }
+        if ($setup->tableExists('sample_news_author')) {
+            $setup->getConnection()->dropTable('sample_news_author');
+        }
+        //remove config settings if any
+        $collection = $this->collectionFactory->create()
+            ->addPathFilter('sample_news');
+        foreach ($collection as $config) {
+            $this->configResource->delete($config);
+        }
+    }
+}
